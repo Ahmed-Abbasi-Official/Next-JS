89c1d4ab63b4095dada6e6669041179388e707d7
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-01-08 19:27:36 +0500

    commit

--- pages/server/users.jsx
+++ pages/server/users.jsx
@@ -1,17 +1,35 @@
+import Link from "next/link";
 
-const users = ({name}) => {
-    console.log(name);
-    
-  return (
-    <div>users</div>
-  )
-}
+const users = ({ res }) => {
+  // console.log(res);
+
+  return <div>
+    <h1>Users</h1>
+    {
+      res.length > 0? (
+        <ul>
+          {res.map((user) => (
+            <li key={user.id}>
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users found</p>
+      )
+    }
+  </div>;
+};
 
 export async function getServerSideProps() {
-    // Fetch data from an API
-    console.log("Server is running");
-   return {}
-    
+  // Fetch data from an API
+  console.log("Server is running");
+  const res = await (await fetch("https://jsonplaceholder.typicode.com/users")).json();
+  return {
+    props: {
+      res,
+    },
+  };
 }
 
-export default users
+export default users;
