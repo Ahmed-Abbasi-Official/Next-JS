0e5b2ded3618908c0c86346bca21cb32055fc06d
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-01-08 23:44:06 +0500

    commit

--- pages/ahmed.js
+++
@@ -1,5 +0,0 @@
-export default function () {
-    return <h4>
-        Ahmed Abbasi
-    </h4>
-}
---
+++ pages/static/users.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+const users = ({ res }) => {
+  return (
+    <div>
+    <h1>Users</h1>
+    {
+      res.length > 0? (
+        <ul>
+          {res.map((user) => (
+            <li key={user.id}>
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users found</p>
+      )
+    }
+  </div>
+  )
+}
+
+export async function getStaticProps() {
+    // Fetch data from an API
+    console.log("Server is running");
+    const res = await (await fetch("https://jsonplaceholder.typicode.com/users")).json();
+    return {
+      props: {
+        res,
+      },
+    };
+  }
+
+export default users
